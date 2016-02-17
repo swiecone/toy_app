65eafb699e8104d1e5e8e12086df925cd671311d
swiecone <alejandro.swiec@gmail.com>
2016-02-18 00:58:47 +0100

    Ammended micropost spec and added factory girl to web app

---
+++ spec/factories/microposts.rb
@@ -0,0 +1,4 @@
+FactoryGirl define do 
+	factory :micropost do 
+		content "AAAAAAAAAAAAAAA"
+	end 
---
+++ spec/factories/users.rb
@@ -0,0 +1,5 @@
+FactoryGirl define do 
+	factory :user do 
+		name "Alex"
+		email "alex@example.com"
+	end 
