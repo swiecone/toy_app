ab33fa211dd75aa24311f2df427440ceed2cd354
swiecone <alejandro.swiec@gmail.com>
2016-02-21 10:17:00 +0100

    Added rest of micropost controllers, which are pending

--- spec/controllers/micropost_controller_spec.rb
+++ spec/controllers/micropost_controller_spec.rb
@@ -15,5 +15,45 @@ describe MicropostsController do
 		end 
 	end	
 
+	describe 'GET #new' do 
+		it "assigns a new Micropost to @user"
+		it "renders the :new template"
+	end 
+
+	describe "GET #edit" do 
+		it "assigns the requested Micropost to @contact"
+		it "renders the :edit template"
+	end 
+
+	describe "POST #create" do 
+		context "with valid attribuetes" do 
+			it "saves the new Micropost in the database"
+			it "redirects to micropost#show"
+		end 
+
+		context "with invalid attributes" do 
+			it "does not save the new Micropost in the database"
+			it "re-renders the :new template"
+		end 
+	end 
+
+	describe 'PATCH #update' do 
+		context "with valid attributes" do 
+			it "updates the Micropost in the database"
+			it "redirects to the contact"
+		end 
+
+		context "with invalid attributes" do
+			it "does not update the Micropost"
+			it "re-renders the :edit template"
+		end 
+	end 
+
+	describe "DELETE #destroy" do 
+		it "deletes the Micropost from the database"
+		it "redirects to users#index"
+	end 
+
+
 
 end # finishes ContactsController 
