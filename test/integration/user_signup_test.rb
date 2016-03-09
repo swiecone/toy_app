9551f348d9e9459ca2e5a1cc512251d4a3721a62
swiecone <alejandro.swiec@gmail.com>
2016-03-09 02:02:45 +0100

    added a test for unsuccessful access and also sessionhelper and login method inside sessionhelper

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -23,5 +23,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
+  test "login with invalid information" do 
+    get login_path
+    assert_template 'session/new'
+    post login_path, session: {email: "", password: "" }
+    assert_template 'session/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end 
+  
+
  
 end
