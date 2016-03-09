6e798695b6958214c7e0d239928b628fa0ffb407
swiecone <alejandro.swiec@gmail.com>
2016-03-09 02:16:44 +0100

    Added the functionality to show name in flash when creating a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+  class UsersController < ApplicationController
   def new
   	@user = User.new
   end
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create 
   	@user = User.new(user_params) # Not the final implementation! 
   	if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome, #{@user.name} to the Sample App!"
   		redirect_to @user
   	else 
   		render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper 
 	# Logs in the given user 
 	def log_in(user)
-		session[:user_id] = user.user_id
+		session[:user_id] = user.id
 	end 
 end 
