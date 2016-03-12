58865e07ab0ae02453272dff5cc97097367ce4e3
swiecone <alejandro.swiec@gmail.com>
2016-03-12 13:40:55 +0100

    started putting together remember me functionality by adding permanent token method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-	  before_save {self.email = email.downcase }
+	  attr_accessor :remember_token
+    before_save {self.email = email.downcase }
 	  validates :name, presence: true, length: { maximum: 50 }
   	  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   	  validates :email, presence: true, length: { maximum: 255 },
@@ -13,4 +14,14 @@ class User < ActiveRecord::Base
                                                   BCrypt::Engine.cost
   		  BCrypt::Password.create(string, cost: cost)
        end
+
+       def user.new_token
+        SecureRandom.urlsafe_base64
+      end
+
+      #remember a user in the database for us in persistent sessions.
+      def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+      end
 end
