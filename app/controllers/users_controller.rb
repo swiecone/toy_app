38d08afb938cce25d804566d89cb31d1c0f6fcef
swiecone <alejandro.swiec@gmail.com>
2016-03-16 00:27:56 +0100

    Added profile update in controller and cehcked its working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,16 @@
     @user = User.find_by(params[:id])
   end 
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end 
+
   private
 
   	def user_params
