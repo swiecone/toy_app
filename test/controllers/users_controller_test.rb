80703ec5a1f0539e38f58ebbcc480982e8f33349
swiecone <alejandro.swiec@gmail.com>
2016-03-20 09:23:19 +0100

    Added tests to redirect user if not logged in when trying to edit or update profile

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -16,6 +16,18 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should redirect edit when not logged in" do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
   test "should create user" do
     assert_difference('User.count') do
       post :create, user: { email: @user.email, name: @user.name }
