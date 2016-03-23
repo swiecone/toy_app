c7894128c3c4830aee09cfe0951b1548ed88645f
swiecone <alejandro.swiec@gmail.com>
2016-03-23 10:33:09 +0100

    test to redirect users that try to see list of users and they have not logged in

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -28,6 +28,11 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to login_url
   end
 
+  test "should redirect index when not logged in" do 
+    get :index 
+    assert_redirected_to login_url
+  end 
+
   test "should create user" do
     assert_difference('User.count') do
       post :create, user: { email: @user.email, name: @user.name }
