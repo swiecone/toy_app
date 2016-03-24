8945b137bbd587e9ac92172dd0971667ecf94976
swiecone <alejandro.swiec@gmail.com>
2016-03-24 07:17:41 +0100

    changed the number of contacts showed per pagge, and also the size of the gravatar image and text

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@
   end
 
   def index 
-        @users = User.all.paginate(page: params[:page], per_page: 10)
+        @users = User.all.paginate(page: params[:page], per_page: 12)
   end 
   
   def create 
