2663754c4ab2e90293b27f3037e75d55bb7cee5a
swiecone <alejandro.swiec@gmail.com>
2016-03-24 08:40:46 +0100

    Added destroy method, and added method to check that user that can delete users is actually an admin before deleting.  If not user, redirected to home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
   class UsersController < ApplicationController
     before_action :logged_in_user, only: [:index, :edit, :update]
     before_action :correct_user, only: [:edit, :update]
+    before_action :admin_user, only: :destroy
 
   def new
   	@user = User.new
@@ -40,7 +41,9 @@
   end 
 
   def destroy 
-   
+   User.find(params[:id]).destroy
+   flash[:success] = "User deleted"
+   redirect_to users_url
   end 
 
   private
@@ -67,4 +70,10 @@
         # grant access to edit section
      end
    end
+
+   # Confirms an user is admin user.  Checked before accessing destroy method!
+   def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end 
+
 end
