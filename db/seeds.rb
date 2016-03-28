fa9bf9571df038001dc9d0a5a766b7aaf2965f7e
swiecone <alejandro.swiec@gmail.com>
2016-03-28 11:12:35 +0200

    Added the activation data to seeds.rb for all other accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,5 +13,7 @@ User.create!(name:  "Example User",
  		 User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now )
 	end
